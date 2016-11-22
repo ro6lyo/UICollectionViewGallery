622cd63e63407bc1f1fe5d670441fe36ed867891
ro6lyo <roshlyo@icloud.com>
2016-11-22 17:55:12 +0200

    file deletions

---
+++ Example/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  Pods
+//
+//  Created by Paulina Simeonova on 11/21/16.
+//
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "InfiniteHorizontalLayout.h"
+#endif /* BridgingHeader_h */
